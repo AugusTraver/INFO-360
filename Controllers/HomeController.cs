5dc6ad7e060cf38e9d75c7eb687b4f1acf8c24dd
AUGUSTO AMADEO TRAVERSO <49315675@ort>
2025-10-09 13:36:22 -0300

    INICIO

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using INFO_360.Models;
+
+namespace INFO_360.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
